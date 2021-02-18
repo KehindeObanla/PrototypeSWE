358cfabaa2eb8c9ec413b978f6d9cf5dbcb3227d
kehinde obanla <kehinde.o.obanla@gmail.com>
2021-02-17 23:46:58 -0600

    tooltip for updatepassword

--- PrototypeSWE/UpdatePass.xaml.cs
+++ PrototypeSWE/UpdatePass.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace PrototypeSWE
 {
@@ -30,33 +31,42 @@ namespace PrototypeSWE
             string seqanswer = SeqAnswer.Text;
             string newpass = NewPass.Text;
             string Renter = RnewPass.Text;
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$";
             if (newpass == Renter)
             {
-                int same = Security.CheckAnswer(username, seqanswer);
-                if (same != 0)
+                if (Regex.IsMatch(newpass, pattern))
                 {
-                   bool update =  Security.Updatetable(newpass, username,same);
-                    if(update == true)
+                    int same = Security.CheckAnswer(username, seqanswer);
+                    if (same != 0)
                     {
-                        wrngpass.Content = "password Updated";
-                        UserName.Clear();
-                        SeqAnswer.Clear();
-                        NewPass.Clear();
-                        RnewPass.Clear();
-                        loginScreen lg = new loginScreen();
-                        lg.Show();
-                        this.Close();
+                        bool update = Security.Updatetable(newpass, username, same);
+                        if (update == true)
+                        {
+                            wrngpass.Content = "password Updated";
+                            UserName.Clear();
+                            SeqAnswer.Clear();
+                            NewPass.Clear();
+                            RnewPass.Clear();
+                            loginScreen lg = new loginScreen();
+                            lg.Show();
+                            this.Close();
+                        }
+                        else
+                        {
+                            wrngpass.Content = "invalid username";
+
+                        }
                     }
                     else
                     {
-                        wrngpass.Content = "invalid username";
-
+                        wrngpass.Content = "False Answer";
                     }
                 }
                 else
                 {
-                    wrngpass.Content = "False Answer";
+                    wrngpass.Content = "Wrong Password Format";
                 }
+                   
             }
             else
             {
